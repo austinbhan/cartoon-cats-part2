23952e90c8a439f70052ad59e27d0bd6de952684
austinbhan <austinbhan@outlook.com>
2022-07-27 14:51:14 -0700

    Deleting data.cats

--- data/cats.js
+++ data/cats.js
@@ -1,10 +1,18 @@
 const { Router } = require('express');
-const cats = require('../models/cats');
+const Cat = require('../lib/models/Dog');
 
 module.exports = Router()
   .get('/:id', async (req, res) => {
     console.log('id param:', req.params.id);
-
+    const cat = await Cat.getById(req.params.id);
+    console.log(cat, 'in cats.js');
+    console.log(cat);
+    res.json(cat);
+  })
+  .get('/', async (req, res) => {
+    const cats = await Cat.getAll();
+    const ids = cats.map((cat) => ({  id: cat.id, name: cat.name }));
+    res.json(ids);
   });
 
-module.exports = { cats };
+// module.exports = router;
